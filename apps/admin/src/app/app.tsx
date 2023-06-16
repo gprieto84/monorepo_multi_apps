112d7c63bebbc248d53b13da9c8f3510f7b8a33f
German Personal <german.prt@gmail.com>
2023-06-15 22:36:10 -0500

    Initial commit

---
+++ apps/admin/src/app/app.tsx
@@ -0,0 +1,13 @@
+
+import { Banner } from '@org/common-ui';
+
+export function App() {
+  return (
+    <>
+      <Banner text="Welcome to our admin app." />
+      <div />
+    </>
+  );
+}
+
+export default App;
